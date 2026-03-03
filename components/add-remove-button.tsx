d4195f06a29fff3a821ce00d3f24c41886cce581
Alanna Tees <joytotheworld188@gmail.com>
2026-03-03 13:16:22 -0500

    button on games list screen shows add/remove based on if the user already owns the game

---
+++ components/add-remove-button.tsx
@@ -0,0 +1,88 @@
+"use client"
+import { createClient } from "@/lib/supabase/client";
+import { Button } from "./ui/button";
+
+function addGame(userGames: number[], gameID: number) {
+  const index = userGames.indexOf(gameID);
+  if (index > -1) {
+    console.log("Game alrady exists in collection");
+    return userGames;
+  } else {
+    return userGames.concat(gameID);
+  }
+}
+
+function removeGame(userGames: number[], gameID: number) {
+  const index = userGames.indexOf(gameID);
+  if (index > -1) {
+    console.log("Game found, removing from collection");
+    userGames.splice(index, 1)
+    return userGames;
+  } else {
+    console.log("Failed to update, game not found in collection");
+  }
+}
+
+export default function AddRemoveButton({ game, isGameInCollection }: { game: Game, isGameInCollection : boolean }) {
+  async function handleClick(gameID: number, isAdd: boolean) {
+    const supabase = await createClient();
+
+    const userName = (await supabase.auth.getUser()).data.user?.user_metadata?.user_name;
+    console.log(userName)
+
+    if (!userName) {
+      console.error('User not found');
+      return;
+    }
+
+    const userGames = (await supabase.from('UserCollectionByUserName').select('*').eq('user_name', userName)).data?.[0]?.user_collection || [];
+    console.log('User Games:', userGames);
+
+    var updatedGames: any = []
+    if (isAdd) {
+      if (!userGames || userGames.length === 0) {
+        console.log('No games found for user, CREATING new collection');
+        const { error: insertError } = await supabase.from('UserCollectionByUserName').insert({ user_name: userName, user_collection: [gameID] });
+        if (insertError) {
+          // TODO: Handle error
+        }
+      }
+
+      console.log('Existing games found for user, UPDATING collection');
+      updatedGames = addGame(userGames, game.id);
+    } else {
+      updatedGames = removeGame(userGames, game.id);
+    }
+
+    const { error: updateError } = await supabase.from('UserCollectionByUserName').update({ user_collection: updatedGames }).eq('user_name', userName);
+    if (updateError) {
+      console.error('Update error:', updateError);
+    } else {
+      console.log('Games updated successfully');
+    }
+
+    const { data: verifyData } = await supabase.from('UserCollectionByUserName').select('user_collection').eq('user_name', userName).single();
+    console.log('Verified games in DB:', verifyData?.user_collection);
+  }
+
+
+  async function getUser() {
+    const supabase = await createClient();
+    const userName = (await supabase.auth.getUser()).data.user?.user_metadata?.user_name;
+
+    const userGames = (await supabase.from('UserCollectionByUserName').select('*').eq('user_name', userName)).data?.[0]?.user_collection || [];
+    console.log('User Games:', userGames);
+
+    return true
+  }
+
+  return !isGameInCollection ? (
+    <div className="flex items-center gap-4">
+      <Button onClick={() => handleClick(game.id, true)}>Add</Button>
+    </div>
+  ) : (
+    <div className="flex items-center gap-4">
+      <Button onClick={() => handleClick(game.id, false)}>Remove</Button>
+    </div>
+  );
+}
--- components/search-games.tsx
+++ components/search-games.tsx
@@ -8,6 +8,7 @@ import GameList from "./game-list";
 export default function SearchGames() {
     const [searchGame, setSearchGame] = useState("");
     const [searchResults, setSearchResults] = useState<Game[]>([]);
+    const [userCollection, setUserCollection] = useState<number[]>([]);
 
     async function handleClick() {
         console.log(searchGame)
@@ -17,6 +18,19 @@ export default function SearchGames() {
         const BoardGames = (await supabase.from("BoardGames").select().textSearch('name', searchGame)).data;
         console.log(BoardGames)
         setSearchResults(BoardGames || [])
+        
+        // Needed to compare against and determine what button to show. Calling here to avoid querying collection for every game
+        getUserCollection();
+    }
+
+
+    async function getUserCollection() {
+        const supabase = await createClient();
+        const userName = (await supabase.auth.getUser()).data.user?.user_metadata?.user_name;
+
+        const userGames = (await supabase.from('UserCollectionByUserName').select('*').eq('user_name', userName)).data?.[0]?.user_collection || [];
+        console.log('User Games:', userGames);
+        setUserCollection(userGames || []);
     }
 
     return (
@@ -35,7 +49,7 @@ export default function SearchGames() {
                 </Button>
             </div>
             <Suspense fallback={<div>Loading Games...</div>}>
-                <GameList games={searchResults} />
+                <GameList games={searchResults} userCollection={userCollection} />
             </Suspense>
         </div>
     );
