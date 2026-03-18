b4f6a263aea09c90a2ceefce22674638dc3aab80
Alanna Tees <joytotheworld188@gmail.com>
2026-03-17 22:57:46 -0400

    updated utils file

--- app/util.tsx
+++ app/util.tsx
@@ -1,33 +1,29 @@
-"use Client";
+"use client";
 
+import { useEffect } from "react";
+import { useAtomValue } from "jotai";
+import { atomEffect } from "jotai-effect";
 import { createClient } from "@/lib/supabase/client";
 import { userCollectionAtom, userNameAtom } from "@/app/store";
-import { useAtomValue } from "jotai";
-import { useCallback } from "react";
 
-export const useUpdateGamesCollection = () => {
-    const supabase = createClient();
-    const userName = useAtomValue(userNameAtom);
-    const userCollection = useAtomValue(userCollectionAtom);
+export function useSyncUserCollection() {
+  const supabase = createClient();
+  const userName = useAtomValue(userNameAtom);
+  const userGames = useAtomValue(userCollectionAtom);
 
-    // TODO: user upsert (combined insert and update)
-    const useUpdateGamesCollection = useCallback(async () => {
-      if (!userCollection) {
-      console.log("No games found for user, CREATING new collection");
-      const { error: insertError } = await supabase.from('UserCollectionByUserName').insert({ user_name: userName, user_collection: userCollection });
-      if (insertError) {
-        console.error("Insert error:", insertError);
+  useEffect(() => {
+    async function sync() {
+      console.log("useEffect trigger")
+      console.log(userGames);
+      const { error } = await supabase.from("UserCollectionByUserName").upsert({user_name: userName, user_collection: userGames});
+
+      if (error) {
+        console.error("Sync error:", error);
       } else {
-        console.log("Games created successfully");
-      }
-    } else {
-      const { error: updateError } = await supabase.from('UserCollectionByUserName').update({ user_collection: userCollection }).eq('user_name', userName);
-      if (updateError) {
-        console.error("Update error:", updateError);
-      } else {
-        console.log("Games updated successfully");
+        console.log("Collection synced");
       }
     }
-  }, [supabase, userName, userCollection]);
-  return useUpdateGamesCollection;
+
+    sync();
+  }, [supabase, userName, userGames]);
 }
