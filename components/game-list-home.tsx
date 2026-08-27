e94e66adc0a378a51d09032c10cad0ae7f8055f1
Alanna Tees <atees@uoguelph.ca>
2026-08-27 17:20:27 -0400

    Updates to home page layout

--- components/game-list-home.tsx
+++ components/game-list-home.tsx
@@ -2,7 +2,7 @@
 
 import { useAtomValue } from 'jotai';
 import { collectionStatusAtom, userGamesAtom } from "@/app/store";
-import { Stack, Divider, Box, Card, CardContent } from '@mui/material';
+import { Stack, Divider, Box, Card, CardContent, Grid } from '@mui/material';
 import AddRemoveGame from './add-remove-game';
 
 export default function GameListHome({
@@ -36,43 +36,40 @@ export default function GameListHome({
   return (
     <ul style={{ display: "flex" }}>
       {games.map((game) => (
-        <Box key={game.id} >
-          <Card sx={{
-            background: "#f6f2f1",
-            marginRight: 2
-          }}>
-            <CardContent>
-              <li style={{ display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center", padding: 5 }}>
-                <Box
-                  sx={{
-                    width: "30",
-                    height: "20vh",
-                    flexShrink: 0
-                  }}
-                >
+          <li key={game.id} style={{ display: "flex", flexDirection: "column", height: "100%", alignItems: "center", justifyContent: "space-between" }}>
+            <Card sx={{
+              background: "#f6f2f1",
+              marginRight: 2,
+              height: "30vh",
+            }}>
+              <CardContent sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+                height: "100%"
+              }}>
+                <Box sx={{ width: "20vh", height: "20vh" }}>
                   <img src={game.image} alt={game.name} style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
                 </Box>
-                <br />
                 <span style={{ fontWeight: 'bold', display: 'block', overflowWrap: 'anywhere', wordBreak: 'break-word' }}>{game.name}</span>
-                <Stack
-                  flex={0.5}
-                  alignItems="center"
-                  justifyContent="center"
-                  sx={{ fontSize: '12px' }}
-                >
-                  {/* TODO: Show on hover or expand or something? */}
-                  {/* <span>{defaultToMinimum(game.minplayers, game.maxplayers)} players</span>
-                  <span>{defaultToMinimum(game.minplaytime, game.maxplaytime)} min</span>
-                  <span>{game.publisher}</span> */}
-                  {Number(game.yearpublished) !== 0 && (
-                    <span style={{ fontSize: '12px' }}>{Number(game.yearpublished)}</span>
-                  )}
+                <Stack sx={{ fontSize: '12px', width: "100%" }}>
+                  <Stack direction={"row"} sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+                    <span style={{ width: "60%" }}>{game.publisher}</span>
+                    {Number(game.yearpublished) !== 0 && (
+                      <span style={{ fontSize: '12px' }}>{Number(game.yearpublished)}</span>
+                    )}
+                  </Stack>
+                  <Stack direction={"row"} sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+                    <span>{defaultToMinimum(game.minplayers, game.maxplayers)}</span>
+                    <span>{defaultToMinimum(game.minplaytime, game.maxplaytime)}</span>
+                  </Stack>
                 </Stack>
-              </li>
-            </CardContent>
-          </Card>
-        </Box>
-      ))}
-    </ul>
+              </CardContent>
+            </Card>
+          </li>
+      ))
+      }
+    </ul >
   );
 }
